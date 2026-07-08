08068f89ad39bb769a9acf7e9cc3358c9f279b5e
TheoCannta <sydneyzuendt@gmail.com>
2026-07-08 14:59:59 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+/*TimeZone project
+
+need time reader
+buch of if then statements
+a way to request a local time and find out that time in thr timezone
+eventually needs an html BUT THIS FIRST*/
+
+public class Main {
+
+    
+}
