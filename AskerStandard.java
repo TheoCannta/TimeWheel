8b9d372e54e4d4e65be6e0b709541c7be16b4ef4
TheoCannta <sydneyzuendt@gmail.com>
2026-07-08 17:21:22 -0400

    start made

--- AskerStandard.java
+++ AskerStandard.java
@@ -5,7 +5,13 @@ buch of if then statements
 a way to request a local time and find out that time in thr timezone
 eventually needs an html BUT THIS FIRST*/
 
-public class Main {
+public class AskerStandard {
+     public AskerStandard(){
+       hourColor = "grey";
+       timeToColor();
+    }
 
-    
+    public String timeToColor(){
+        return "sup";
+    }
 }
