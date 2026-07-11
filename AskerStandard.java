7aa82634bd6e42e90a63ae24552f38da89cb005e
TheoCannta <sydneyzuendt@gmail.com>
2026-07-11 18:22:20 -0400

    started work on converting time to color
    realised i need to figure out APIs silly

--- AskerStandard.java
+++ AskerStandard.java
@@ -1,17 +1,26 @@
 /*TimeZone project
 
+Author- THeo Cannta
+
 need time reader
 buch of if then statements
 a way to request a local time and find out that time in thr timezone
 eventually needs an html BUT THIS FIRST*/
 
+
 public class AskerStandard {
-     public AskerStandard(){
+    String hourColor;//asker time uses colors instead of numbers for compatibility
+
+
+    public AskerStandard(){
        hourColor = "grey";
-       timeToColor();
+       convertToColor();
+    }
+    //takes current time and replaces the number with a color
+    public static String convertToColor(){
+
+        return "grey";
     }
 
-    public String timeToColor(){
-        return "sup";
-    }
+    
 }
