b0bcd1ba08496dcc2b8557da836c984382485101
TheoCannta <sydneyzuendt@gmail.com>
2026-07-14 17:11:07 -0400

    UGGGGGGGGGGGGGGGGGGGGGG

--- AskerStandard.java
+++ AskerStandard.java
@@ -1,3 +1,4 @@
+
 /*TimeZone project
 
 Author- THeo Cannta
@@ -5,11 +6,11 @@ Author- THeo Cannta
 need time reader
 buch of if then statements
 a way to request a local time and find out that time in thr timezone
-eventually needs an html BUT THIS FIRST*/
+eventually needs an html BuT THIS FIRST*/
 
 
 public class AskerStandard {
-    String hourColor;//asker time uses colors instead of numbers for compatibility
+    String hourColor="blah blah blah";//asker time uses colors instead of numbers for compatibility
 
 
     public AskerStandard(){
